5ab383193568e6793455ba978a50edaf6b0c17db
RubenTT91 <ruben.toro1@gmail.com>
2024-11-18 23:24:15 -0500

    Se crea el movimiento y la limpieza del circulo

--- .idea/function.js
+++ .idea/function.js
@@ -3,16 +3,16 @@ const ctx = canvas.getContext('2d') // se selecciona para que renderice en 2d
 
 // tamaño de canvas
 canvas.width = 448
-canvas.heigth   = 400
+canvas.heigth   = 600
 
 // variables de la pelota
-const ballRadius =8
+const ballRadius = 2
 let x = canvas.width / 2
-let y = canvas.heigth -30
+let y = canvas.heigth -460
 
 // velocidad de la pelota
-let dx = 2
-let dy = -2
+let dx = 1
+let dy = -1
 
 
 function drawBall(){
@@ -29,13 +29,18 @@ function drawBricks(){}
 
 function collisionDetection(){}
 function ballMoveement(){
-
+    x += dx
+    y += dy
 }
 function paddleMovemeent(){}
+function clearDraw(){
+    ctx.clearRect(0,0, canvas.width, canvas.height)
 
+}
 
 function draw(){
     //funciones para dibujar
+    clearDraw()
     drawBall()
     //console.log("hola mundo")
     drawPaddle()
