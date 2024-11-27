c4da0fdba4ba4e53f51f1da510faa2f8d7c02e6e
rubencho <ruben.toro1@gmail.com>
2024-11-26 23:56:48 -0500

    Se ajusta el game Over si toca el suelo

--- .idea/function.js
+++ .idea/function.js
@@ -1,73 +1,68 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d') // se selecciona para que renderice en 2d
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d"); // se selecciona para que renderice en 2d
 
 // tamaño de canvas
-canvas.width = 448
-canvas.heigth   = 600
+canvas.width = 448;
+canvas.heigth = 600;
 
 // variables de la pelota
-const ballRadius = 2
-let x = canvas.width / 2
-let y = canvas.heigth -460
+const ballRadius = 2;
+let x = canvas.width / 2;
+let y = canvas.heigth - 460;
 
 // velocidad de la pelota
-let dx = -2
-let dy = -1
+let dx = -2;
+let dy = -1;
 
-
-function drawBall(){
-    ctx.beginPath()
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
-    ctx.fillStyle = '#eee'
-    ctx.fill()
-    ctx.closePath()
-
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#eee";
+  ctx.fill();
+  ctx.closePath();
 }
-function drawPaddle(){}
-function drawBricks(){}
-function drawBricks(){}
+function drawPaddle() {}
+function drawBricks() {}
+function drawBricks() {}
 
-function collisionDetection(){
-if(
-    x + dx > canvas.width -ballRadius ||
-    x + dx < ballRadius
-){
-    dx = -dx
-}
+function collisionDetection() {
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
 
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  }
 
- if 
- (
-    y +dy < ballRadius)
- { dy = -dy}
-
+  if (y + dy > canvas.height) {
+    console.log("Game Over");
+    document.location.reload();
+  }
 }
 
-function ballMoveement(){
-    x += dx
-    y += dy
+function ballMoveement() {
+  x += dx;
+  y += dy;
 }
-function paddleMovemeent(){}
+function paddleMovemeent() {}
 
-function clearDraw(){
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-
+function clearDraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function draw(){
-    //funciones para dibujar
-    clearDraw()
-    drawBall()
-    //console.log("hola mundo")
-    drawPaddle()
-    drawBricks()
+function draw() {
+  //funciones para dibujar
+  clearDraw();
+  drawBall();
+  //console.log("hola mundo")
+  drawPaddle();
+  drawBricks();
 
-    // Coliciones
-    collisionDetection()
-    ballMoveement()
-    paddleMovemeent()
+  // Coliciones
+  collisionDetection();
+  ballMoveement();
+  paddleMovemeent();
 
-    window.requestAnimationFrame(draw) //Es un metodo que programa una función para que se ejecute antes de que se antes de que se refresque la ventana. Es un loop infinito
-
- }
- draw()
+  window.requestAnimationFrame(draw); //Es un metodo que programa una función para que se ejecute antes de que se antes de que se refresque la ventana. Es un loop infinito
+}
+draw();
