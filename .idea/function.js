0d9ead525f2ea60a916a9caf8dc9943960c3a03d
rubencho <ruben.toro1@gmail.com>
2024-12-03 22:47:58 -0500

    Ordena el código y se mueve las funciones de Colisiones a movimiento

--- .idea/function.js
+++ .idea/function.js
@@ -1,4 +1,4 @@
-const canvas = document.querySelector("canvas")
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // se selecciona para que renderice en 2d
 
 // tamaño de canvas
@@ -9,7 +9,6 @@ canvas.heigth = 400;
 const ballRadius = 2;
 let x = canvas.width / 2;
 let y = canvas.heigth - 250;
-
 
 // variables del paddlet
 
@@ -29,89 +28,87 @@ function drawBall() {
 }
 
 // Variables del paddlet
-const paddletHeigh = 3;
+const paddletHeigh = 5;
 const paddletWith = 30;
-
 let paddleX = (canvas.width - paddletWith) / 2;
 let paddley = canvas.height - paddletHeigh - 5;
 
+//Dibujar paddlet o recuadro
 function drawPaddle() {
-  ctx.fillStyle = "green";
-  ctx.fillRect(
-    paddleX,
-    paddley,
-    paddletWith,
-    paddletHeigh
-  )
-
+  ctx.fillStyle = "red";
+  ctx.fillRect(paddleX, paddley, paddletWith, paddletHeigh);
 }
-function drawBricks() { }
-function drawBricks() { }
+function drawBricks() {}
+function drawBricks() {}
 
 function initEvent() {
-  document.addEventListener('keydown', UserKeyPress);
-  document.addEventListener('keyup', UserKeyUp);
+  document.addEventListener("keydown", KeyPressed);
 
+  document.addEventListener("keyup", KeyUp);
 
-  function UserKeyPress(event) {
+  // escucho el evento cuando presionan la tecla
+  function KeyPressed(event) {
     const { key } = event;
-
-// escucho el evento cuando presionan la tecla
-
     if (key == "Right" || key == "ArrowRight") {
-      paddletRight = true
+      paddletRight = true;
     } else if (key == "Left" || key == "ArrowLeft") {
-      paddletLeft = true
+      paddletLeft = true;
     }
-// escucho el evento cuando sueltan la tecla
-    function UserKeyUp(event) {
-      const { key } = event;
-      if (key == "Right" || key == "ArrowRigth") {
-        paddletRight = falase;
+  }
+  // escucho el evento cuando sueltan la tecla
+  function KeyUp(event) {
+    const { key } = event;
+    if (key == "Right" || key == "ArrowRigth") {
+      paddletRight = false;
+    } else if (key == "Left" || key == "ArrowLeft") {
+      paddletLeft = false;
+    }
+  }
+}
 
-      } else if (key == "Left" || key == "ArrowLeft") {
-        paddletLeft = false;
-      }
-    }
-  }}
+function collisionDetection() {
+}
 
-  function collisionDetection() {
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-      dx = -dx;
-    }
-
-    if (y + dy < ballRadius) {
-      dy = -dy;
-    }
-
-    console.log("Game Over");
-    document.location.reload();
-    }
+function ballMoveement() {
   
-
-  function ballMoveement() {
-    x += dx;
-    y += dy;
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
   }
-  function paddleMovemeent() { }
-
-  function clearDraw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (y + dy < ballRadius) {
+    dy = -dy;
   }
 
-  function draw() {
-    //funciones para dibujar
-    clearDraw();
-    drawBall();
-    //console.log("hola mundo")
-    drawPaddle();
-    drawBricks();
-
-    // Coliciones
-    collisionDetection();
-    ballMoveement();
-    paddleMovemeent();
-
-    window.requestAnimationFrame(draw) //Es un metodo que programa una función para que se ejecute antes de que se antes de que se refresque la ventana. Es un loop infinito
+  //Colision de juego terminado porque toca el suelo
+  
+  if (y + dy > canvas.height) {
+    document.location.reload();
   }
-  draw();
+  x += dx;
+  y += dy;
+}
+function paddleMovemeent() {}
+
+function clearDraw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function draw() {
+  console.log(paddletLeft, paddletRight);
+
+  //funciones para dibujar
+  clearDraw();
+  drawBall();
+  //console.log("hola mundo")
+  drawPaddle();
+  drawBricks();
+
+  // Coliciones
+  collisionDetection();
+  ballMoveement();
+  paddleMovemeent();
+
+  window.requestAnimationFrame(draw); //Es un metodo que programa una función para que se ejecute antes de que se antes de que se refresque la ventana. Es un loop infinito
+}
+
+draw();
+initEvent();
